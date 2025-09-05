ff1ce5e0b1b23a356a87daab4e371c42bf568cb0
Abdullah Rafi <75572904+Totalradium@users.noreply.github.com>
2025-09-05 23:05:52 +0000

    .

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export SECRET_KEY="django-insecure-$(openssl rand -base64 32)"
-export DEBUG=False
-export PORT=${PORT:-10000}
-python manage.py migrate
-python manage.py collectstatic --noinput
-gunicorn website.wsgi:application --bind 0.0.0.0:${PORT}
